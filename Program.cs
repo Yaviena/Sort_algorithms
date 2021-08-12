b5f7b04a6adf70bcbb77a0d0406d9b6dc076cf8c
Magda Szafranska <magdalena.szafranska@outlook.com>
2021-08-12 13:59:49 +0200

    add RANDOM NUMBERS method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace Sort_algorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("How many numbers do you want to sort: ");
+            int howManyNumbers = int.Parse(Console.ReadLine());
+            Random_Numbers(howManyNumbers);
+        }
+
+        private static int [] Random_Numbers(int nr)
+        {
+            int[] tab = new int[nr];
+
+            Random rnd = new Random();
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] = rnd.Next(20);
+            }
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.Write("{0,5}", tab[i]);
+            }
+            return tab;
         }
     }
 }
