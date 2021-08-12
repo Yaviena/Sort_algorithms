d4d8ef576b6679ea9139f2397dcfbe62270ae006
Magda Szafranska <magdalena.szafranska@outlook.com>
2021-08-12 13:37:31 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sort_algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
